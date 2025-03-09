9ca14ee84ba5bbf9eeaa92d91d083c14cc49a357
Breana <brecjohnson@gmail.com>
2025-03-09 00:01:26 -0500

    added user model & connected server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+/* MONGOOSE SET UP */
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  })
+  .catch((err) => console.log(`error: ${err}`));
