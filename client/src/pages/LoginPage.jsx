40b347c6f380173b2f3e092bf047c64bf5e60ce2
Breana <brecjohnson@gmail.com>
2025-03-10 00:17:40 -0400

    added login page, updated background images

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import '../styles/Login.scss'
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className='login'>
-      <h1>LOGIN</h1>
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Register Here</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
